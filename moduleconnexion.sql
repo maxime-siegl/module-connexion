6169a8cba1ed3c3f7e0add9d1358ca7570e9ea90
maxime <maxime.siegl@laplateforme.io>
2020-05-24 12:00:46 +0200

    maj bdd

--- moduleconnexion.sql
+++ moduleconnexion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  sam. 23 mai 2020 à 13:14
+-- Généré le :  Dim 24 mai 2020 à 10:00
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -36,16 +36,16 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `nom` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `utilisateurs`
 --
 
 INSERT INTO `utilisateurs` (`id`, `login`, `prenom`, `nom`, `password`) VALUES
-(1, 'admin', 'admin', 'admin', 'admin'),
-(3, 'Wojdeen', 'Jeremy', 'Graffin', 'lui'),
-(2, 'Maxime', 'Maxime', 'Siegl', 'Moi');
+(1, 'admin', 'admin', 'admin', '$2y$10$cMRu/F4NkoMMASrhlbQCZeQhuSm7bXiJuk32MKTu7eYoueGugHlSu'),
+(2, 'Maxime', 'Maxime', 'Siegl', '$2y$10$cW0VolMqLl9t0tdXMNPiWOEDAarIgg6Z5j.rZdgnnVFU69jv9WFjC'),
+(3, 'test', 'test', 'test', '$2y$10$WslK/Col5I9tzQdPzwlhfur7IRTmKHQiJlRdFsapF5mpEjLrQ19PS');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
